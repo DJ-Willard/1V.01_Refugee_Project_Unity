8130ab0d21b4ee6c4a995af98f2dc24cae2a01c7
thewilloflandru <87150774+thewilloflandru@users.noreply.github.com>
2023-05-26 10:26:37 -0700

    Fixed ObjectiveHandler to reset objective on SceneManager.LoadScene(0)

--- Lifeline_in_the_Blind/Assets/Scriptable Objects/ObjectiveHandler/ObjectiveHandler.cs
+++ Lifeline_in_the_Blind/Assets/Scriptable Objects/ObjectiveHandler/ObjectiveHandler.cs
@@ -6,6 +6,10 @@ using TMPro;
 [CreateAssetMenu(fileName = "New ObjectiveHandler", menuName = "Inventory System/ObjectiveHandler")]
 public class ObjectiveHandler : ScriptableObject
 {
+    // REFACTORED FROM QUEUE TO NORMAL LIST
+    // Easier to handle with by just holding onto an index in the list.
+    // Allows easier reset during something like SceneManager.LoadScene(0)
+
     // MainObjectives is dependent list of objectives, must go in order
     // Side is independent (could do list of queues for more complex dependencies,
     // such as quests.
@@ -14,11 +18,13 @@ public class ObjectiveHandler : ScriptableObject
 
     public List<ObjectiveItem> MainObjList = new List<ObjectiveItem>();
     public List<ObjectiveItem> SideObjList = new List<ObjectiveItem>(); // placeholder
+    public int CurrentMainObjIndex;
     private Queue<ObjectiveItem> MainObjQueue = new Queue<ObjectiveItem>();
 
     // called by ThirdPersonController in Start()
     public void Init()
     {
+        /* QUEUE VERSION
         // convert public list to private queue
         for (int i = 0; i < MainObjList.Count; i++)
         {
@@ -27,10 +33,15 @@ public class ObjectiveHandler : ScriptableObject
         }
         // get first objective from queue
         GetNextMainObjective();
+        */
+
+        // LIST VERSION
+        CurrentMainObjIndex = 0;
     }
 
     public void GetNextMainObjective()
     {
+        /* QUEUE VERSION
         // Set initial objective from queue
         if (MainObjQueue.TryDequeue(out CurrentMainObj)){
             //objectiveTextPanel.GetComponent<TextMeshProUGUI>().text = CurrentMainObj.ToString();
@@ -38,20 +49,41 @@ public class ObjectiveHandler : ScriptableObject
         } else {
             Debug.Log("No remaining MainObj in queue.");
         }
+        */
+
+        // LIST VERSION
+        if (CurrentMainObjIndex < MainObjList.Count)
+        {
+            CurrentMainObjIndex++;
+            Debug.Log("MainObj index successfully set.");
+        } else {
+            // reset index here or no? 
+            Debug.Log("No remaining MainObj in list.");
+        }
     }
 
     // Review: ref keyword allows passing of var
     public void DisplayCurrObjectiveByRef(ref TMP_Text newtext)
     {
         Debug.Log("ObjectiveHandler.cs: DCPBR() called.");
-        newtext.text = CurrentMainObj.objectiveText;
+        
+        // QUEUE VERSION
+        // newtext.text = CurrentMainObj.objectiveText;
+
+        // LIST VERSION
+        newtext.text = MainObjList[CurrentMainObjIndex].objectiveText;
     }
 
     public string GetCurrObjText()
     {
         Debug.Log("ObjectiveHandler.cs: GetCurrObjText() called.");
-        Debug.Log(CurrentMainObj.objectiveText);
-        return CurrentMainObj.objectiveText;
+        
+        // QUEUE VERSION
+        // Debug.Log(CurrentMainObj.objectiveText);
+        // return CurrentMainObj.objectiveText;
+
+        // LIST VERSION
+        return MainObjList[CurrentMainObjIndex].objectiveText;
     }
 
     public void Quit()
@@ -59,5 +91,6 @@ public class ObjectiveHandler : ScriptableObject
         // May or may not want to do this depending on how level transfers work.
         // Doesn't hurt to have a placeholder quit function. 
         CurrentMainObj = null;
+        CurrentMainObjIndex = 0;
     }
 }
