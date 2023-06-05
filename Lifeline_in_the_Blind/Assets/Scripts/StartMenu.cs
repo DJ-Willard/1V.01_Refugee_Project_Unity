bf08f35300a5fb5e147bf02f4cc568199d6d3c86
thewilloflandru <87150774+thewilloflandru@users.noreply.github.com>
2023-06-05 14:32:33 -0700

    Working on Start Menu. StartMenu scene seems to be messing up control scheme for main scene.

---
+++ Lifeline_in_the_Blind/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // options to load checkpoint in other functions?
+        // weird scene loading problems. 
+        // 1. StartMenu scene seems to be holding onto controls in some way
+        // 2. Lighting on game scene is not right
+        // SceneManager.UnloadSceneAsync(0); // makes no difference, async or not
+        SceneManager.LoadScene(1, LoadSceneMode.Single);    // single mode should unload all other scenes anyways, and is default
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
